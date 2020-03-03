22863d1bed6817ee98fa5500059430fa1046c464
Fu <fu.goto@gmail.com>
2020-03-03 22:53:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function takeANumber(katzDeliLine, newName) {
-  katzDeliLine.push(newName)
-  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
+//function takeANumber(katzDeliLine, newName) {
+//  katzDeliLine.push(newName)
+//  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in/ line.`
+//}
+
+function takeANumber(katzDeliLine) {
+  var newNumber = katzDeliLine.length+1
+  katzDeliLine.push(newNumber)
+  return ` You are number ${newNumber} in line.`
 }
+
+console.log(takeANumber(katzDeliLine))
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {return "There is nobody waiting to be served!"}
