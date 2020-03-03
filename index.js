e156972c6cb414a2013376686cbcb10b37dad6e0
Fu <fu.goto@gmail.com>
2020-03-03 23:06:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,14 +5,21 @@
 
 var katzDeliLine = []
 
+//function takeANumber(katzDeliLine) {
+//  var newNumber = katzDeliLine.length+1
+//  katzDeliLine.push(newNumber)
+//  return `You are number ${newNumber} in line.`
+//}
+//console.log(takeANumber(katzDeliLine))
+//console.log(takeANumber(katzDeliLine))
+//console.log(takeANumber(katzDeliLine))
+var counter = 0
+
 function takeANumber(katzDeliLine) {
-  var newNumber = katzDeliLine.length+1
-  katzDeliLine.push(newNumber)
-  return `You are number ${newNumber} in line.`
+  counter++
+  katzDeliLine.push(counter)
+  return `You are number ${counter} in line.`
 }
-console.log(takeANumber(katzDeliLine))
-console.log(takeANumber(katzDeliLine))
-console.log(takeANumber(katzDeliLine))
 
 
 
@@ -20,6 +27,8 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {return "There is nobody waiting to be served!"}
   else {return `Currently serving ${katzDeliLine.shift()}.`}
 }
+
+
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length===0) {return "The line is currently empty."}
